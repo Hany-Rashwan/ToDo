0cc7c7a2dc51067489aa6cfdebe1769e317cf3e8
unknown <hany-rashwan.softenghanyr@gmail.com>
2018-08-07 13:03:30 +0200

    deply update1

--- client/main.js
+++ client/main.js
@@ -1,7 +1,7 @@
 import { Template } from 'meteor/templating';
 //import { ReactiveVar } from 'meteor/reactive-var';
 
-import '../imports/body.js';
+import '../imports/ui/body.js';
 
 
   // counter starts at 0
--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,12 +1,12 @@
 import { Template } from 'meteor/templating';
  
 import './body.html';
-import { Tasks } from './api/tasks.js';
+import { Tasks } from '../api/tasks.js';
 
  
 Template.task.helpers({
   tasks() {
-     return Tasks.find();
+     return Tasks.find({}, { sort: { createdAt: -1 } });
  },
 });
 
@@ -28,4 +28,17 @@ Template.body.events({
     // Clear form
     target.text.value = '';
   },
+
+   'click .toggle-checked'() {
+    // Set the checked property to the opposite of its current value
+    Tasks.update(this._id, {
+      $set: { checked: ! this.checked },
+    });
+  },
+
+  'click .delete'() {
+    Tasks.remove(this._id);
+  }
+  
 });
+
